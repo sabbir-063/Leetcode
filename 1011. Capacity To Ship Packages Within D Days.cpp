5f0ecef1d89e2c05a0c3511ddafc2546ba2bfaa4
sabbir-063 <114491259+sabbir-063@users.noreply.github.com>
2023-02-22 22:27:19 +0600

    Leetcode Solution c++
    Leetcode 1011 || Capacity To Ship Packages Within D Days || C++ Solution || Binary Search

---
+++ 1011. Capacity To Ship Packages Within D Days.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool possible(vector<int>&wt, int cap, int days){
+        int dd=1, cnt=0;
+        
+        for(auto i : wt){
+            cnt += i;
+            if(cnt > cap) dd++, cnt=i;
+        }
+        
+        return dd <= days;
+    }
+    
+    int shipWithinDays(vector<int>& weights, int days) {
+        int mx=0, cnt=0;
+        for(auto i : weights) mx=max(mx, i), cnt += i;
+        int l=mx, r=cnt;
+        
+        while(l < r){
+            int mid = (l+r) >> 1;
+            if(possible(weights, mid, days)) r = mid;
+            else l=mid+1;
+        }
+        
+        return l; 
+    }
+};
