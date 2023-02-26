b5bac6a501c6ef34080581fcd8f30665e5da5d1a
sabbir-063 <114491259+sabbir-063@users.noreply.github.com>
2023-02-26 09:02:32 +0600

    Leetcode 72 Solution || Day 56 || 2023 || C++

---
+++ 72. Edit Distance.cpp
@@ -0,0 +1,34 @@
+/*
+Problem : https://leetcode.com/problems/edit-distance/
+
+Author : Sabbir Musfique
+Time Complexity : O(m*n)
+Space Complexity : O(m*n)
+*/
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m=word1.size();
+        int n=word2.size();
+
+        int dp[m+1][n+1];
+        for(int i=0; i<=m; i++) dp[i][0]=i;
+        for(int i=0; i<=n; i++) dp[0][i]=i;
+
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                if(word1[i-1] == word2[j-1]) dp[i][j] = dp[i-1][j-1];
+                else{
+                    int insrt = 1+dp[i][j-1];
+                    int delet = 1+dp[i-1][j];
+                    int replce = 1+dp[i-1][j-1];
+
+                    dp[i][j] = min({insrt, delet, replce});
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+};
